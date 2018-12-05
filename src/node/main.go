90b95e4a5b1563cac0da844828e5fd51ae6b7c2a
Salil Kapur <salilkapur93@gmail.com>
2018-12-05 00:19:09 -0500

    Removing debug statements Fixing formatting

--- src/node/main.go
+++ src/node/main.go
@@ -7,37 +7,33 @@ package main
 
 import (
 	"fmt"
-	"github.com/google/uuid"
 	"log"
 	"net"
-	//"google.golang.org/grpc"
-	//"github.com/Divvy/src/pb"
-)
 
-// PeerT stores information about other Divvy peers
-type PeerT struct {
-	ID      uuid.UUID
-	Address net.IP
-}
+	//"google.golang.org/grpc"
+
+	//"github.com/Divvy/src/pb"
+	"github.com/google/uuid"
+)
 
 // NodeT stores information about this node
 type NodeT struct {
-	ID     uuid.UUID
-	netMgr *NetworkManager
+	ID      uuid.UUID
+	netMgr  *NetworkManager
+	fileMgr *FileManager
 
 	// List of Divvy peers
 	peers []PeerT
-
-	// Create file Manager
-	fileMgr *FileManager
 }
 
 // Initialize a new NodeT
 func newNodeT() NodeT {
 	Node := NodeT{}
 	Node.ID = uuid.New()
+
 	// Make an empty list of peers
 	Node.peers = make([]PeerT, 0)
+
 	return Node
 }
 
@@ -49,24 +45,16 @@ func initNode(Node *NodeT) {
 	Node.netMgr = NewNetworkManager()
 	Node.fileMgr = NewFileManager("/home/vagrant/go/src/github.com/Divvy/test")
 
-	// Read configuration
-	// Initialize file manager
-
 	log.Printf("[Node] Divvy node initialized!")
 }
 
 // Main function that handles all requests from sub-services
 func main() {
-	fmt.Println("jnkjnjn")
 	// Initialize node
 	Node := newNodeT()
 
 	initNode(&Node)
 
-	log.Printf("path is %s", Node.fileMgr.files[0].Path)
-	log.Printf("file exists is %v", Node.fileMgr.files[0].exists(Node.fileMgr.files[0].Path))
-	log.Printf("hash is %s", Node.fileMgr.files[0].GetHash(Node.fileMgr.files[0].Path))
-	Node.fileMgr.displayDirectory()
 	/*
 	   Once everything is setup start listening. This call is blocking
 	   Do not put any logic after gRPC serve
--- src/node/network.go
+++ src/node/network.go
@@ -2,43 +2,48 @@ package main
 
 import (
 	"errors"
+	"log"
 	"net"
-    "log"
 
-    context "golang.org/x/net/context"
-    "google.golang.org/grpc"
+	"github.com/google/uuid"
+	context "golang.org/x/net/context"
+	"google.golang.org/grpc"
 
-    "github.com/Divvy/src/pb"
+	"github.com/Divvy/src/pb"
 )
 
 // Colon is not a typo!
 const (
-    controlPort = ":2018"
+	controlPort = ":2018"
 )
+
+// PeerT stores information about other Divvy peers
+type PeerT struct {
+	ID      uuid.UUID
+	Address net.IP
+}
 
 // NetworkManger implements the Divvy interface
 type NetworkManager struct {
-    address             net.IP
-    availableToOthers   bool
-    grpcServer          *grpc.Server
+	address           net.IP
+	availableToOthers bool
+	grpcServer        *grpc.Server
+
+	// List of Divvy peers
+	peers []PeerT
 }
 
-func NewNetworkManager() (*NetworkManager) {
-    netMgr := &NetworkManager{}
-    netMgr.getLocalAddress()
+func NewNetworkManager() *NetworkManager {
+	netMgr := &NetworkManager{}
+	netMgr.getLocalAddress()
 
 	log.Printf("[Network] IP: %v", netMgr.address)
-    netMgr.grpcServer = grpc.NewServer()
+	netMgr.grpcServer = grpc.NewServer()
 
-    // This seems confusing. Is there a better way of doing this?
-    pb.RegisterDivvyServer(netMgr.grpcServer, netMgr)
+	// This seems confusing. Is there a better way of doing this?
+	pb.RegisterDivvyServer(netMgr.grpcServer, netMgr)
 
-    return netMgr
-}
-
-func (netmgr *NetworkManager) Ping(ctx context.Context, empty *pb.Empty) (*pb.Success, error) {
-    log.Printf("[Network] Received ping")
-    return &pb.Success{}, nil
+	return netMgr
 }
 
 func (netMgr *NetworkManager) getLocalAddress() (net.IP, error) {
@@ -47,13 +52,13 @@ func (netMgr *NetworkManager) getLocalAddress() (net.IP, error) {
 	if err != nil {
 		goto ERROR
 	}
-	// handle err
+
 	for _, i := range ifaces {
 		addrs, err := i.Addrs()
-		// handle err
 		if err != nil {
 			goto ERROR
 		}
+
 		for _, addr := range addrs {
 			var ip net.IP
 			switch v := addr.(type) {
@@ -65,21 +70,33 @@ func (netMgr *NetworkManager) getLocalAddress() (net.IP, error) {
 			if ip == nil || ip.IsLoopback() {
 				continue
 			}
-            // Supports on IPv4
+			// Supports only IPv4
 			ip = ip.To4()
 			if ip == nil {
 				continue
 			}
-            netMgr.address = ip
-            netMgr.availableToOthers = true
+			netMgr.address = ip
+			netMgr.availableToOthers = true
 			return ip, nil
 		}
 	}
 
 ERROR:
-    netMgr.availableToOthers = false
+	netMgr.availableToOthers = false
 	if err != nil {
 		return nil, err
 	}
 	return nil, errors.New("[Network Manager] Unable to get local address")
+}
+
+// Divvy service interface implementation
+
+func (netMgr *NetworkManager) Ping(ctx context.Context, empty *pb.Empty) (*pb.Success, error) {
+	log.Printf("[Network] Received ping")
+	return &pb.Success{}, nil
+}
+
+func (netMgr *NetworkManager) NodeJoin(ctx context.Context, newNode *pb.NewNode) (*pb.Success, error) {
+	// A new peer has appeared
+	return &pb.Success{}, nil
 }
